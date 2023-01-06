147adc940aafe2da94d6b0fbc8c60701f92dc51e
Sethhhhhhh <yohannvvt@gmail.com>
2023-01-06 16:55:12 +0100

    fix user module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -4,10 +4,11 @@ import { UsersController } from './users.controller';
 import { PrismaService } from 'src/prisma.service';
 import { UsersGateway } from './users.gateway';
 import { JwtService } from '@nestjs/jwt';
+import { AuthService } from 'src/auth/auth.service';
 
 @Module({
     controllers: [UsersController],
-    providers: [UsersService, PrismaService, JwtService, UsersGateway],
+    providers: [UsersService, PrismaService, AuthService, JwtService, UsersGateway],
     exports: [UsersService]
 })
 export class UsersModule {}
